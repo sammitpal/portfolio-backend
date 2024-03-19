88a22315e2fb96cc7564b1df153f6f04248c342a
Sammit Pal <sammit.pal3920@gmail.com>
2024-03-19 19:48:17 +0530

    Increased Maintainability

--- src/main/java/com/portfolio/service/SessionServiceImpl.java
+++ src/main/java/com/portfolio/service/SessionServiceImpl.java
@@ -44,32 +44,32 @@ public class SessionServiceImpl {
 	private String fetchClientIP() {
 		// Retrieve the client's IP address from the X-Forwarded-For header if present
 		ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-		if (attributes != null) {
-			HttpServletRequest request = attributes.getRequest();
-			String ipAddress = request.getHeader("X-Forwarded-For");
-			// If X-Forwarded-For header is not present, fall back to remote address
-			if (ipAddress == null || ipAddress.isEmpty()) {
-				ipAddress = request.getHeader("X-Real-IP");
-				if (ipAddress == null || ipAddress.isEmpty()) {
-					ipAddress = request.getHeader("Proxy-Client-IP");
-					if (ipAddress == null || ipAddress.isEmpty()) {
-						ipAddress = request.getHeader("WL-Proxy-Client-IP");
-						if (ipAddress == null || ipAddress.isEmpty()) {
-							ipAddress = request.getHeader("HTTP_CLIENT_IP");
-							if (ipAddress == null || ipAddress.isEmpty()) {
-								ipAddress = request.getHeader("HTTP_X_FORWARDED_FOR");
-								if (ipAddress == null || ipAddress.isEmpty()) {
-									ipAddress = request.getRemoteAddr();
-								}
-							}
-						}
-					}
-				}
-			}
-			return ipAddress;
-		} else {
-			return ""; // Handle null case
+		if (attributes == null) {
+		    return ""; // Handle null case
 		}
+
+		HttpServletRequest request = attributes.getRequest();
+		String ipAddress = request.getHeader("X-Forwarded-For");
+		if (ipAddress == null || ipAddress.isEmpty()) {
+		    ipAddress = request.getHeader("X-Real-IP");
+		}
+		if (ipAddress == null || ipAddress.isEmpty()) {
+		    ipAddress = request.getHeader("Proxy-Client-IP");
+		}
+		if (ipAddress == null || ipAddress.isEmpty()) {
+		    ipAddress = request.getHeader("WL-Proxy-Client-IP");
+		}
+		if (ipAddress == null || ipAddress.isEmpty()) {
+		    ipAddress = request.getHeader("HTTP_CLIENT_IP");
+		}
+		if (ipAddress == null || ipAddress.isEmpty()) {
+		    ipAddress = request.getHeader("HTTP_X_FORWARDED_FOR");
+		}
+		if (ipAddress == null || ipAddress.isEmpty()) {
+		    ipAddress = request.getRemoteAddr();
+		}
+
+		return ipAddress;
 	}
 
 
@@ -98,9 +98,7 @@ public class SessionServiceImpl {
 			} else {
 				return false;
 			}
-		} catch (IOException e) {
-			throw new SessionExpiredException(e.getLocalizedMessage());
-		} catch (JsonParseException e) {
+		} catch (IOException | JsonParseException e) {
 			throw new SessionExpiredException(e.getLocalizedMessage());
 		}
 	}
