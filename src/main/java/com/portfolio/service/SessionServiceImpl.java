461bd47d7e37afc46e75fbd82d33671e24783ec5
Sammit Pal <sammit.pal3920@gmail.com>
2024-03-19 19:56:47 +0530

    Increased Maintainability

--- src/main/java/com/portfolio/service/SessionServiceImpl.java
+++ src/main/java/com/portfolio/service/SessionServiceImpl.java
@@ -24,7 +24,6 @@ public class SessionServiceImpl {
 	@Autowired
 	private HttpServletRequest request;
 
-
 	public String createSession() {
 		ObjectMapper obj = new ObjectMapper();
 		String jsonString = "";
@@ -45,38 +44,37 @@ public class SessionServiceImpl {
 		// Retrieve the client's IP address from the X-Forwarded-For header if present
 		ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
 		if (attributes == null) {
-		    return ""; // Handle null case
+			return ""; // Handle null case
 		}
 
 		HttpServletRequest request = attributes.getRequest();
 		String ipAddress = request.getHeader("X-Forwarded-For");
 		if (ipAddress == null || ipAddress.isEmpty()) {
-		    ipAddress = request.getHeader("X-Real-IP");
+			ipAddress = request.getHeader("X-Real-IP");
 		}
 		if (ipAddress == null || ipAddress.isEmpty()) {
-		    ipAddress = request.getHeader("Proxy-Client-IP");
+			ipAddress = request.getHeader("Proxy-Client-IP");
 		}
 		if (ipAddress == null || ipAddress.isEmpty()) {
-		    ipAddress = request.getHeader("WL-Proxy-Client-IP");
+			ipAddress = request.getHeader("WL-Proxy-Client-IP");
 		}
 		if (ipAddress == null || ipAddress.isEmpty()) {
-		    ipAddress = request.getHeader("HTTP_CLIENT_IP");
+			ipAddress = request.getHeader("HTTP_CLIENT_IP");
 		}
 		if (ipAddress == null || ipAddress.isEmpty()) {
-		    ipAddress = request.getHeader("HTTP_X_FORWARDED_FOR");
+			ipAddress = request.getHeader("HTTP_X_FORWARDED_FOR");
 		}
 		if (ipAddress == null || ipAddress.isEmpty()) {
-		    ipAddress = request.getRemoteAddr();
+			ipAddress = request.getRemoteAddr();
 		}
 
 		return ipAddress;
 	}
 
-
 	public Boolean verifyToken(String token) {
 		byte[] decode = Base64.getDecoder().decode(token);
 		String decodedString = new String(decode);
-		
+
 		Boolean verified = false;
 
 		String currentIP = fetchClientIP().toString().trim();
@@ -85,14 +83,9 @@ public class SessionServiceImpl {
 		try {
 			Date currentDate = new Date();
 			Session session = mapper.readValue(decodedString, Session.class);
-			if (session != null) {
-				if (session.getExpDate().compareTo(currentDate) > -1) {
-					if (currentIP.equals(session.getIp())) {
-						verified=true;
-					}
-					
-				}
-			} 
+			if (session != null && session.getExpDate().compareTo(currentDate) > -1 && currentIP.equals(session.getIp())) {
+				verified = true;
+			}
 			return verified;
 		} catch (IOException | JsonParseException e) {
 			throw new SessionExpiredException(e.getLocalizedMessage());
